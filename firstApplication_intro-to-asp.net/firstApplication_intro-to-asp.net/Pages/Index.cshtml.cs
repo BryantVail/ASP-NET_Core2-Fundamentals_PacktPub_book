500cfd3f5ce7e10c9b74e9647ce9587d24089006
Bryant Vail <BryantVail@gmail.com>
2019-03-28 17:50:55 -0400

    init project through VStudio

---
+++ firstApplication_intro-to-asp.net/firstApplication_intro-to-asp.net/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace firstApplication_intro_to_asp.net.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
