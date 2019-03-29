e4a212b6dea99929470982a1eb51fa635ba05357
Bryant Vail <BryantVail@gmail.com>
2019-03-29 17:49:36 -0400

    init

---
+++ firstApplication_intro-to-asp.net/WebApplication1/Controllers/Hello3Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lesson2
+{
+    public class Hello3Controller : Controller
+    {
+        // GET: /<controller>/
+
+        public ActionResult Welcome(int id)
+        {
+            /*example: 
+             *
+             *  http://localhost/Hello3/Welcome/1
+             */
+            return View();
+        }
+
+        //ActionResult Welcome()
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        //ActionResult Greeting
+        public ActionResult Greeting(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ firstApplication_intro-to-asp.net/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lesson2
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult /*string*/ Index()
+        {
+            //return View();
+            //return "Hello World, I am going to master MVC";
+            return Content("Hellow World from Index, returning Content(string msg)");
+        }
+
+        public IActionResult IndexUpper()
+        {
+            return new UpperStringActionResult("Hello World! I'm learning MVC, upper");
+        }
+    }
+    //public class Home2Controller : Controller
+    //{
+    //    // GET: /<controller>/
+    //    //public /*IActionResult*/ string Index()
+    //    //{
+    //    //    //return View();
+    //    //    return "Hello World, I am going to master MVC, Home1";
+    //    //}
+
+    //    public IActionResult Index()
+    //    {
+    //        return Content("Hellow World from Content()");
+    //    }
+    //}
+
+}
