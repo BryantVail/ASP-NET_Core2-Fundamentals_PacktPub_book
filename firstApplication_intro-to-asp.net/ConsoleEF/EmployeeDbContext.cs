da82e7277b30a88881a10282d7394d28a23a03e2
Bryant Vail <Bryanttv@outlook.com>
2019-06-10 03:32:19 -0400

    working on Add-Migration

---
+++ firstApplication_intro-to-asp.net/ConsoleEF/EmployeeDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ConsoleEF
+{
+    class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"
+                Data Source=localhost\SQLEXPRESS; 
+                Initial Catalog=EFConsole; 
+                Integrated Security=True;
+                Connect Timeout=30;
+                Encrypt=False;
+                TrustServerCertificate=True;
+                ApplicationIntent=ReadWrite;
+                MultiSubnetFailover=False
+            ");
+        }
+    }
+}
---
+++ firstApplication_intro-to-asp.net/ConsoleEF/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleEF.Models
+{
+    class Employee
+    {
+        // className + "Id" will be considered as primary key by EF
+        public int EmployeeId { get; set; }
+        //strings will be auto created as nvarchar(max) by default
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Designation { get; set; }
+
+    }
+}
